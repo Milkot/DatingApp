0dce16b521d10a1fd0c1280b6e9d6265ab0f05d5
Milkot <mrebray@laposte.net>
2020-08-30 12:03:57 +0200

    Ajout de libraries tierces, ajout de style

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = "http://localhost:5000/api/auth/";
+  baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
-constructor(private http: HttpClient) {
-
+  constructor(private http: HttpClient) {
 }
+
   login(model: any){
     return this.http.post(this.baseUrl + 'login', model)
     .pipe(
@@ -17,7 +21,8 @@ constructor(private http: HttpClient) {
         const user = response;
         if (user){
           localStorage.setItem('token', user.token);
-          console.log(user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          console.log(this.decodedToken);
         }
       })
     );
@@ -26,5 +31,10 @@ constructor(private http: HttpClient) {
   register(model: any)
   {
     return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Beetic';
+  jwtHelper = new JwtHelperService();
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+
+    // Permet de décoder et d'accèder au token JWT à chaque chargement de l'appli
+    // Authentification ++
+    const token = localStorage.getItem('token');
+    if (token)
+    {
+      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
